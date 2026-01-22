c28e8c9082c8f2c45c163758cc2bebcf68fe7f1e
mziyabo-glitch <mziyabo@gmail.com>
2026-01-22 15:57:26 +0000

    Create event.tsx

---
+++ app/event.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { AppText } from "@/components/ui/AppText";
+
+export default function EventScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <AppText>Event screen coming next</AppText>
+    </View>
+  );
+}
